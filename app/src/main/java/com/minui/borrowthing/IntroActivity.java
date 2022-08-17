0c30dba00eedbbf5dc02e5eb7f4bf154ebbd991a
ghkfugks <105832380+ghkfugks@users.noreply.github.com>
2022-08-17 13:01:15 +0900

    add

---
+++ app/src/main/java/com/minui/borrowthing/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.minui.borrowthing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        gotoMain(1);  // 파라미터 만큼 delay가 된다.
+
+    }
+
+    private void gotoMain(int sec) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000 * sec);
+    }
+}
