4ac7fcdff11600bf6ad145f2a61be5fa9f7b2890
ghkfugks <105832380+ghkfugks@users.noreply.github.com>
2022-08-17 16:50:19 +0900

    add

--- app/src/main/java/com/minui/borrowthing/MyInformation.java
+++ app/src/main/java/com/minui/borrowthing/MyInformation.java
@@ -2,12 +2,28 @@ package com.minui.borrowthing;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MyInformation extends AppCompatActivity {
+
+    Button btnInformation;
+    Button btnInterestsList;
+    Button btnMyWrote;
+    Button btnMyLocation;
+    Button btnTransaction;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_information);
+
+        btnInformation = findViewById(R.id.btnInformation);
+        btnInterestsList = findViewById(R.id.btnInterestsList);
+        btnMyWrote = findViewById(R.id.btnMyWrote);
+        btnMyLocation = findViewById(R.id.btnMyLocation);
+        btnTransaction = findViewById(R.id.btnTransaction);
+        btnLogout = findViewById(R.id.btnLogout);
+
     }
 }
