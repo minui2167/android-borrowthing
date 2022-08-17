4948633b87d073b812f7edf75d6b215bd24da4f2
ghkfugks <105832380+ghkfugks@users.noreply.github.com>
2022-08-17 17:29:52 +0900

    add

--- app/src/main/java/com/minui/borrowthing/MyInformation.java
+++ app/src/main/java/com/minui/borrowthing/MyInformation.java
@@ -2,6 +2,7 @@ package com.minui.borrowthing;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MyInformation extends AppCompatActivity {
@@ -24,6 +25,50 @@ public class MyInformation extends AppCompatActivity {
         btnMyLocation = findViewById(R.id.btnMyLocation);
         btnTransaction = findViewById(R.id.btnTransaction);
         btnLogout = findViewById(R.id.btnLogout);
+
+
+
+        btnInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo 내정보 수정하는 화면 구성
+            }
+        });
+
+        btnInterestsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo 내 관심 목록 불러오기
+            }
+        });
+
+        btnMyWrote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo 내가 쓴 글 목록 불러오기
+            }
+        });
+
+        btnMyLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo 내 현재 위치 설정
+            }
+        });
+
+        btnTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo 거래내역 불러오기
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo 로그아웃 구성
+            }
+        });
 
     }
 }
