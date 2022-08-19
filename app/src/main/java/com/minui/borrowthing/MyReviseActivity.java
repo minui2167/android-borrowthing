42be508608382af8b64c863906da925bb339ccc0
ghkfugks <105832380+ghkfugks@users.noreply.github.com>
2022-08-19 10:51:22 +0900

    내정보 수정 화면 구성

---
+++ app/src/main/java/com/minui/borrowthing/MyReviseActivity.java
@@ -0,0 +1,14 @@
+package com.minui.borrowthing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyReviseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_revise);
+    }
+}
